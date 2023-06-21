19764c399cefa2e1399edf8e22b874d6618d77dc
Juan Pablo de Jesus Figueroa Jaramillo <juanpablodejesusfigueroa@gmail.com>
2023-06-21 13:14:27 -0600

    Subiendo cambios ya tenemos el erp

--- index.php
+++ index.php
@@ -1,23 +1,72 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-  <?php include "static/head.php"; ?>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+  <title>Sistema ERP - C&ME Logistics</title>
+  <!-- Simple bar CSS -->
+  <link rel="stylesheet" href="assets/css/simplebar.css">
+  <!-- Fonts CSS -->
+  <link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <!-- Icons CSS -->
+  <link rel="stylesheet" href="assets/css/feather.css">
+  <!-- Date Range Picker CSS -->
+  <link rel="stylesheet" href="assets/css/daterangepicker.css">
+  <!-- App CSS -->
+  <link rel="stylesheet" href="assets/css/app-light.css" id="lightTheme">
+  <link rel="stylesheet" href="assets/css/styles.css" id="lightTheme">
+  <link rel="stylesheet" href="assets/css/app-dark.css" id="darkTheme" disabled>
 </head>
 
-<body>
-  <div id="app">
-    <?php include "components/LoginForm.php"; ?>
+<body class="light ">
+  <div class="wrapper vh-100">
+    <div class="row align-items-center h-100">
+      <form class="col-lg-3 col-md-4 col-10 mx-auto text-center">
+        <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="./index.html">
+          <img src="assets/images/logo_cme.png" alt="Logo" class="logo_inicio">
+        </a>
+        <h1 class="h6 mb-3">Iniciar Sesión</h1>
+        <div class="form-group">
+          <label for="inputEmail" class="sr-only">Correo Eléctronico</label>
+          <input type="email" id="inputEmail" class="form-control form-control-lg" placeholder="Email address" required="" autofocus="">
+        </div>
+        <div class="form-group">
+          <label for="inputPassword" class="sr-only">Password</label>
+          <input type="password" id="inputPassword" class="form-control form-control-lg" placeholder="Password" required="">
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Iniciar Sesión</button>
+        <p class="mt-5 mb-3 text-muted">C&ME Logistics © 2023</p>
+      </form>
+    </div>
   </div>
-  <script src="assets/modules/jquery.min.js"></script>
-  <script src="assets/modules/popper.js"></script>
-  <script src="assets/modules/tooltip.js"></script>
-  <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-  <script src="assets/modules/moment.min.js"></script>
-  <script src="assets/js/stisla.js"></script>
-  <script src="assets/js/scripts.js"></script>
-  <script src="assets/js/custom.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/moment.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/simplebar.min.js"></script>
+  <script src='js/daterangepicker.js'></script>
+  <script src='js/jquery.stickOnScroll.js'></script>
+  <script src="js/tinycolor-min.js"></script>
+  <script src="js/config.js"></script>
+  <script src="js/apps.js"></script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-56159088-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'UA-56159088-1');
+  </script>
+</body>
+
+</html>
 </body>
 
 </html>
--- static/head.php
+++ static/head.php
@@ -1,12 +1,24 @@
-<meta charset="UTF-8" />
-<meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport" />
-<title>Ecommerce Dashboard &mdash; Stisla</title>
-<link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css" />
-<link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css" />
-<link rel="stylesheet" href="assets/modules/jqvmap/dist/jqvmap.min.css" />
-<link rel="stylesheet" href="assets/modules/summernote/summernote-bs4.css" />
-<link rel="stylesheet" href="assets/modules/owlcarousel2/dist/assets/owl.carousel.min.css" />
-<link rel="stylesheet" href="assets/modules/owlcarousel2/dist/assets/owl.theme.default.min.css" />
-<link rel="stylesheet" href="assets/css/style.css" />
-<link rel="stylesheet" href="assets/css/components.css" />
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="favicon.ico">
+<title>Tiny Dashboard - A Bootstrap Dashboard Template</title>
+<!-- Simple bar CSS -->
+<link rel="stylesheet" href="assets/css/simplebar.css">
+<!-- Fonts CSS -->
+<link href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<!-- Icons CSS -->
+<link rel="stylesheet" href="assets/css/feather.css">
+<link rel="stylesheet" href="assets/css/select2.css">
+<link rel="stylesheet" href="assets/css/dropzone.css">
+<link rel="stylesheet" href="assets/css/uppy.min.css">
+<link rel="stylesheet" href="assets/css/jquery.steps.css">
+<link rel="stylesheet" href="assets/css/jquery.timepicker.css">
+<link rel="stylesheet" href="assets/css/quill.snow.css">
+<!-- Date Range Picker CSS -->
+<link rel="stylesheet" href="assets/css/daterangepicker.css">
+<!-- App CSS -->
+<link rel="stylesheet" href="assets/css/app-light.css" id="lightTheme">
+<link rel="stylesheet" href="assets/css/styles.css" id="lightTheme">
+<link rel="stylesheet" href="assets/css/app-dark.css" id="darkTheme" disabled>
