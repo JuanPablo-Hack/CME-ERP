412be740f092211d8aca245d83d288d87b88a24b
Juan Pablo <juanpablodejesusfigueroa@gmail.com>
2023-08-19 13:57:50 -0600

    Subiendo los modulos de los demás

---
+++ Operador/static/head.php
@@ -0,0 +1,28 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="Apartado de administrador de sistemas ERP de CME Logistics">
+<meta name="author" content="Jupiter Soft Tech">
+<link rel="icon" href="../assets/images/logo_cme.png">
+<title>Admin - ERP CME Logistics</title>
+<!-- Simple bar CSS -->
+<link rel="stylesheet" href="../assets/css/simplebar.css">
+<!-- Fonts CSS -->
+<link
+    href="https://fonts.googleapis.com/css2?family=Overpass:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
+<!-- Icons CSS -->
+<link rel="stylesheet" href="../assets/css/feather.css">
+<link rel="stylesheet" href="../assets/css/select2.css">
+<link rel="stylesheet" href="../assets/css/dropzone.css">
+<link rel="stylesheet" href="../assets/css/uppy.min.css">
+<link rel="stylesheet" href="../assets/css/jquery.steps.css">
+<link rel="stylesheet" href="../assets/css/jquery.timepicker.css">
+<link rel="stylesheet" href="../assets/css/quill.snow.css">
+<!-- Date Range Picker CSS -->
+<link rel="stylesheet" href="../assets/css/daterangepicker.css">
+<!-- App CSS -->
+<link rel="stylesheet" href="../assets/css/app-light.css" id="light Theme">
+<link rel="stylesheet" href="../assets/css/styles.css" id="lightTheme">
+<link rel="stylesheet" href="../assets/css/app-dark.css" id="darkTheme" disabled>
+<link rel="stylesheet" href="../assets/css/dataTables.bootstrap4.css" />
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
